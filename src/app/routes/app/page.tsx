b23a88aa818166a11728556e873b6ca12018c499
Ka Bun <bun2932005@gmail.com>
2025-03-12 22:55:42 +0700

    test: support for test brand-popular component

---
+++ src/app/routes/app/page.tsx
@@ -0,0 +1,9 @@
+import { PopularBrands } from "src/components/ui/brands/popular-brands";
+
+export const Test = () => {
+  return (
+    <div className="">
+      <PopularBrands />
+    </div>
+  );
+};
