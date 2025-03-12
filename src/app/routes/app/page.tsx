a177cbf5387ae834a331286ddc332ecc77774a18
Ka Bun <bun2932005@gmail.com>
2025-03-13 00:07:02 +0700

    test: support for test footer UI

--- src/app/routes/app/page.tsx
+++ src/app/routes/app/page.tsx
@@ -1,9 +1,11 @@
+import { AppFooter } from "src/components/layouts/app-layout/app-footer";
 import { PopularBrands } from "src/components/ui/brands/popular-brands";
 
 export const Test = () => {
   return (
-    <div className="">
+    <div className="flex gap-4 flex-col px-25 py-12">
       <PopularBrands />
+      <AppFooter />
     </div>
   );
 };
---
+++ src/constant/footer-content.ts
@@ -0,0 +1,40 @@
+export const FIRST_COLUMN_CONTENT = [
+  {
+    title: "Impressum",
+  },
+  {
+    title: "FAQ",
+  },
+  {
+    title: "About Us",
+  },
+  {
+    title: "Merchants",
+  },
+  {
+    title: "Brands",
+  },
+];
+
+export const SECOND_COLUMN_CONTENT = [
+  {
+    title: "Our Services",
+  },
+  {
+    title: "Comparison Shopping Platform",
+  },
+  {
+    title: "Managed Ads",
+  },
+  {
+    title: "Premium CSS",
+  },
+  {
+    title: "Register Webshop",
+  },
+  {
+    title: "Contact Us",
+  },
+];
+
+export const LOGO_FOOTER = ["facebook", "linkedin", "twitter", "instagram"] as const;
