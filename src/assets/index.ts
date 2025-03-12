a83c374014571e999f0fe43d8ae7abd9661e6451
Ka Bun <bun2932005@gmail.com>
2025-03-12 22:52:47 +0700

    resources: setup images for popular brands #1

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,9 @@
 import logo_icon from "./logo.svg?react";
+import apple_brand from "./apple_brand.svg?react";
+import nike_brand from "./nike_brand.svg?react";
+import dyson_brand from "./dyson_brand.svg?react";
+import lego_brand from "./lego_brand.svg?react";
+import aeg_brand from "./aeg_brand.svg?react";
+import michael_kors_brand from "./michael_kors_brand.svg?react";
 
-export { logo_icon };
+export { logo_icon, apple_brand, nike_brand, dyson_brand, lego_brand, aeg_brand, michael_kors_brand };
