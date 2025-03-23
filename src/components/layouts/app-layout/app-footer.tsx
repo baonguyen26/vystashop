ec20495e062a510ef2a443a5cd54028038491245
Ka Bun <bun2932005@gmail.com>
2025-03-23 16:14:58 +0700

    style: responsive footer #1

--- src/components/layouts/app-layout/app-footer.tsx
+++ src/components/layouts/app-layout/app-footer.tsx
@@ -9,39 +9,44 @@ import {
 export const AppFooter = () => {
   return (
     <footer>
-      <div className="py-13 px-[100px] flex justify-between">
-        <div className="flex gap-20">
+      <div className="py-13 px-[20px] md:px-[60px] lg:px-[100px] flex flex-col md:flex-row lg:flex-row justify-between">
+        <div className="flex flex-col gap-5 md:flex-row lg:flex-row md:gap-20">
           <div className="flex flex-col gap-4">
             <p className="font-medium">Vysta</p>
             {FIRST_COLUMN_CONTENT.map((item, index) => (
-              <Link
-                to={item.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-sm"
-                key={index}
-              >
-                {item.title}
-              </Link>
+              <div className="pl-4 md:pl-0">
+                <Link
+                  to={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-sm"
+                  key={index}
+                >
+                  {item.title}
+                </Link>
+              </div>
             ))}
           </div>
 
           <div className="flex flex-col gap-4">
             <p className="font-medium">Vysta Business</p>
             {SECOND_COLUMN_CONTENT.map((item, index) => (
-              <Link
-                to={item.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-sm"
-                key={index}
-              >
-                {item.title}
-              </Link>
+              <div className="pl-4 md:pl-0">
+                <Link
+                  to={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-sm"
+                  key={index}
+                >
+                  {item.title}
+                </Link>
+              </div>
             ))}
           </div>
         </div>
-        <div className="flex flex-col items-center gap-5">
+
+        <div className="flex flex-col pt-10 md:pt-0 items-center gap-5">
           <div className="flex gap-4">
             {LOGO_FOOTER_CONTENT.map((item, index) => (
               <Link
@@ -70,8 +75,8 @@ export const AppFooter = () => {
           </Link>
         </div>
       </div>
-      <div className="py-8">
-        <p className="text-center text-[##3A4B55]">
+      <div className="py-6 px-[20px] md:px-[60px]">
+        <p className="text-xs text-center text-[##3A4B55]">
           Copyright © 2020-2025 -Vysta. -
           <Link
             to="https://www.google.com/"
